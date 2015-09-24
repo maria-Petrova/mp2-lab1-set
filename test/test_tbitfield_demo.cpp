a8f96d75e6e6187e6111da673906d29bc9d566ba
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2015-09-24 21:35:58 +0300

    Add parameterized test

--- test/test_tbitfield_demo.cpp
+++ test/test_tbitfield_demo.cpp
@@ -63,3 +63,37 @@ TEST_F(TBitField_Demo_Fixture, all_odd_bits_are_unset)
         EXPECT_EQ(0, bf_.GetBit(i));
     }
 }
+
+class Parameterized_TBitField_Demo : public ::testing::TestWithParam<int> {
+  // You can implement all the usual fixture class members here.
+  // To access the test parameter, call GetParam() from class
+  // TestWithParam<T>.
+};
+
+TEST_P(Parameterized_TBitField_Demo, can_create_bitfield_with_different_len)
+{
+    int len = GetParam();
+
+    TBitField bf(len);
+
+    EXPECT_EQ(len, bf.GetLength());
+}
+
+TEST_P(Parameterized_TBitField_Demo, can_set_different_bits)
+{
+    TBitField bf(100);
+
+    int idx = GetParam();
+    EXPECT_EQ(0, bf.GetBit(idx));
+
+    bf.SetBit(idx);
+    EXPECT_NE(0, bf.GetBit(idx));
+}
+
+INSTANTIATE_TEST_CASE_P(Even,
+                        Parameterized_TBitField_Demo,
+                        ::testing::Values(2, 4, 8));
+
+INSTANTIATE_TEST_CASE_P(Odd,
+                        Parameterized_TBitField_Demo,
+                        ::testing::Values(3, 7, 13));
