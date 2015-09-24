822b494b997bbf72b5f717dda64a92f1add9122b
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2015-09-24 21:17:38 +0300

    Add test fixture

--- test/test_tbitfield_demo.cpp
+++ test/test_tbitfield_demo.cpp
@@ -23,3 +23,43 @@ TEST(TBitField_Demo, can_get_length)
 
     EXPECT_EQ(10, bf.GetLength());
 }
+
+// A new one of these is created for each test
+class TBitField_Demo_Fixture : public testing::Test
+{
+public:
+  TBitField bf_;
+
+  TBitField_Demo_Fixture() : bf_(10) {}
+
+  virtual void SetUp()
+  {
+      for (int i = 1; i < bf_.GetLength(); i += 2)
+      {
+          bf_.SetBit(i);
+      }
+  }
+
+  virtual void TearDown() {}
+};
+
+TEST_F(TBitField_Demo_Fixture, can_get_length)
+{
+    EXPECT_EQ(10, bf_.GetLength());
+}
+
+TEST_F(TBitField_Demo_Fixture, all_even_bits_are_set)
+{
+    for (int i = 1; i < bf_.GetLength(); i += 2)
+    {
+        EXPECT_NE(0, bf_.GetBit(i));
+    }
+}
+
+TEST_F(TBitField_Demo_Fixture, all_odd_bits_are_unset)
+{
+    for (int i = 0; i < bf_.GetLength(); i += 2)
+    {
+        EXPECT_EQ(0, bf_.GetBit(i));
+    }
+}
